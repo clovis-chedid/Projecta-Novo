a46936b5a296fe0bb83be924d0b7c1c55f6e89f4
Davi <davif127@gmail.com>
2018-12-05 13:49:42 -0200

    Testando

--- index.php
+++ index.php
@@ -13,53 +13,8 @@
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Hello, world!</title>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Culturinvest</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>    
-  </head>
-  <body>
-    <div class="mx-auto" style="width: 200px;">
-        <p class="text-white">asd</p>
-    </div>
-    <div class="mx-auto" style="width: 200px;">
-        <p class="text-white">asd</p>
-    </div>
-    <div class="mx-auto" style="width: 200px;">
-        <p class="text-white">asd</p>
-    </div>
-    <div class="mx-auto" style="width: 200px;">
-        <p class="text-white">asd</p>
-    </div>
-    <div class="mx-auto" style="width: 200px;">
-        <p class="text-white">asd</p>
-    </div>
-    <div class="container-fluid bg-dark">
-        <p class="text-white">asd</p>
-    </div>
     
+  </head>
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
