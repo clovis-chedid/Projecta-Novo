83417e37eb74a3def6c0ad97fb9901ded3fb977f
Davi <davif127@gmail.com>
2018-12-07 13:34:15 -0200

    Adicionado favicon

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
   <!doctype html>
   <html lang="pt">
     <head>
-
+      <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <style>
